27f7a21d8b9616f371fbb50ea1efe816919ef096
Amonrat Pongka <102948521+P-Amonrat@users.noreply.github.com>
2024-05-24 07:22:29 +0700

    resolve set state after 5 seconds

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import FruitsCard from './components/FruitsCard';
 import ListCard from './components/ListCard';
@@ -10,33 +10,13 @@ function App() {
   const [itemList, setItemList] = useState(list)
 
   const handleClickItem = (item) => {
-    const checkItem = selectedItem.findIndex((selected) => selected.name === item.name)
-
-    if (checkItem === -1) {
-      setSelectedItem([...selectedItem, { ...item, time: 5 }])
-      setItemList(itemList.filter((listItem) => listItem.name !== item.name))
-    } else {
-      setSelectedItem(selectedItem.filter((_, index) => index !== checkItem))
-      setItemList([...itemList, item])
-    }
+    setSelectedItem((prev) => [...prev, item]);
+    setItemList((prev) => prev.filter((i) => i.name !== item.name));
+    setTimeout(() => {
+        setSelectedItem((prev) => prev.filter((i) => i.name !== item.name));
+        setItemList((prev) => [...prev, item]);
+    }, 5000);
   }
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      selectedItem.map(item => {
-        if (item.time === 0) {
-          const [items, ...rest] = selectedItem
-          setSelectedItem(rest)
-          setItemList([...itemList, items])
-        } else {
-          item.time -= 1
-        }
-      })
-    }, 1000);
-
-    return () => clearInterval(interval);
-
-  }, [selectedItem]);
 
   return (
     <div className="App">
